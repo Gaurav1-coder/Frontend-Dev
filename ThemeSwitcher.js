8a44eb8e1a2b9a2e359b20d6451371de3d5ab2bc
Gaurav Katiyar <katiyargaurav358@user.noreply.github.com>
2025-11-30 10:44:31 +0530

    Q1: Implemented Library Management logic including add, search, and list books functionality

--- ThemeSwitcher.js
+++
@@ -1,8 +0,0 @@
-const btns = document.querySelectorAll(".theme-btn");
-
-btns.forEach((b) =>
-  b.addEventListener("click", () => {
-    const theme = b.dataset.theme;
-    document.body.setAttribute("data-theme", theme);
-  })
-);
